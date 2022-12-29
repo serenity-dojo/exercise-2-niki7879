b8752002dbb50fda1d061f358f20ccfd12dec6ac
madaga <madhulika.agarwal@kindredgroup.com>
2022-12-29 11:52:54 +0000

    Exercise2

--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
@@ -16,9 +16,10 @@ public class WhenWorkingWithFloatingPointNumbers {
         double celcius = 27.0d;
         double farenheit = 0.0d;
 
-        // TODO: Use a floating point calculation to calculate the farenheit equivalent of the celcius value.
+        double fer = (9.0 / 5) * celcius + 32;
 
-        assertThat(farenheit, equalTo(80.6));
+        // TODO: Use a floating point calculation to calculate the farenheit equivalent of the celcius value.
+        assertThat(fer, equalTo(80.6));
     }
 
     //
@@ -27,12 +28,14 @@ public class WhenWorkingWithFloatingPointNumbers {
     //
     @Test
     public void convertMetersToFeet() {
-        int weightInKilograms = 50;
-        double weightInPounds = 0;
+        double weightInKilograms = 50.0d;
+        double weightInPounds = 0.0d;
+
+       double weightInPoundsnew = weightInKilograms * 2.20462;
 
         // TODO: Use a floating point calculation to calculate the correct weight in pounds
 
-        assertThat(weightInPounds, equalTo(110.231));
+        assertThat(weightInPoundsnew, equalTo(110.231));
 
     }
 }
--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithWholeNumbers.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithWholeNumbers.java
@@ -11,10 +11,10 @@ public class WhenWorkingWithWholeNumbers {
     public void addingNumbersTogether() {
         int initialYear = 1985;
         int targetYear = 0;
-
+        int timeJump = 30;
         // TODO: create a new int variable called timeJump and assign it a value
         //  Next, add this variable to initialYear and assign the result to targetYear, so that targetYear is equal to 2015
-
-        assertThat(targetYear, equalTo(2015));
+          targetYear = initialYear + timeJump;
+          assertThat(targetYear, equalTo(2015));
     }
 }
